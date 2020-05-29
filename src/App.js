625fbbc25ecc128b5e10af80f6dd98c2b8fd51c9
tanaymittal25 <tanay.mittal25@gmail.com>
2020-05-29 21:33:19 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path = '/' component = { HomePage } />
     </div>
   );
 }
